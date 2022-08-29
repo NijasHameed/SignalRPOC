c73dad8badfd354adaec57bfdef144a6d3e6cf69
nijasmon.ah@phases.dk <nijasmon.ah@phases.dk>
2022-08-29 21:10:17 +0530

    Push Notification

---
+++ SignalRPOC/SignalRPOC/Controllers/ServerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRPOC.Hubs;
+using SignalRPOC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRPOC.Controllers
+{
+    public class ServerController : Controller
+    {
+        private readonly IHubContext<NotificationHub> _notificationHub;
+        public ServerController(IHubContext<NotificationHub> notificationHub)
+        {
+            _notificationHub = notificationHub;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(Notification model)
+        {
+            await _notificationHub.Clients.All.SendAsync("RecieveMsg", model.Message);
+            return View();
+        }
+
+    }
+}
---
+++ SignalRPOC/SignalRPOC/wwwroot/js/ClientNotification.js
@@ -0,0 +1,10 @@
+﻿"use strict"
+var connection = new signalR.HubConnectionBuilder().withUrl("/notificationHub").build();
+connection.start();
+connection.on("RecieveMsg", function (msg) {
+
+    var li = document.createElement("li");
+    li.textContent = msg;
+    document.getElementById("msgList").appendChild(li);
+
+});
